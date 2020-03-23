898d9991769f969476861a0d2dbb435931a247bc
Zac Smith <zrs3141592@gmail.com>
2020-03-23 10:46:55 -0600

    update app and dotenv for HASH_ROUNDS

--- DOTENV-EXAMPLE.md
+++ DOTENV-EXAMPLE.md
@@ -1,5 +1,10 @@
+## This is an example of the current app dotenv file
+### Copy it and set your own enviroment variables
+
 PORT=6555
 DB_ENV=development
+JWT_SECRET
+HASH_ROUNDS
 
 GITHUB_ID=
 GITHUB_SECRET=
--- users/auth-router.js
+++ users/auth-router.js
@@ -6,14 +6,15 @@ const passport = require('passport')
 const Users = require("./users-model.js");
 
 // --------------- GitHub using passport ------------------
-  router.get('/github', passport.authenticate('github', {}))
+router.get('/github', passport.authenticate('github', {}))
 
 
 
-   // for endpoints beginning with /api/auth
+// for endpoints beginning with /api/auth
 router.post("/register", validateUserContent, (req, res) => {
   let user = req.body;
-        const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+  const HASH_ROUNDS = process.env.JWT_HASH_ROUNDS || 8
+  const hash = bcrypt.hashSync(user.password, HASH_ROUNDS); // 2 ^ n
   user.password = hash;
 
   Users.add(user)
